1aa151b4058343dcd636f53180f3d05a1e8adf71
valentina buoro <valentinabuoro4@gmail.com>
2024-05-13 22:27:28 +0100

    wip

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -92,71 +92,23 @@ async function getProductByName(req, res) {
   }
 }
 
-async function updateProduct() {}
+async function updateProduct(req, res) {
+  const product = await ProductModel.findOne(req.params.productName)
+  if (product){
+    const { productName, price, description, sideEffects, category, quantity } = product
+    product.productName = req.body.productName || productName
+    product.price = req.body.price || price
+    product.description = req.body.description || description
+    product.sideEffects = req.body.sideEffects || sideEffects
+    product.category = req.body.category || category
+    product.quantity = req.body.quantity || quantity
+    
+  }
+}
+
 
 async function deleteProduct() {}
 
-async function createNewProduct(req, res) {
-    try {
-       
-        console.log(res.body)
-        const {productName, quantity } = req.body;
-       //console.log(req.body)
-       let cart = await CartModel.findById(user);
-       console.log(cart)
-       /*if (!cart) {
-         res.status(400).json({
-           success: false,
-           message: "Cart not found",
-         });
-       }
-       res.status(200).json({
-         success: true,
-         message: "Product created successfully",
-         cart,
-       });
-      /*const { productName, price, description, sideEffects, category, quantity } =
-        req.body;
-      if (
-        !productName ||
-        !price ||
-        !description ||
-        !sideEffects ||
-        !category ||
-        !quantity
-      ) {
-        res.status(400).json({
-          success: false,
-          message: "Please provide all required fields",
-        });
-      }
-      const productExists = await ProductModel.findOne({ productName });
-      if (productExists) {
-        res.status(400).json({
-          success: false,
-          message: "A product with this name already exists",
-        });
-      }
-      const newProduct = await ProductModel.create({
-        productName,
-        price,
-        description,
-        sideEffects,
-        category,
-        quantity,
-      });
-      res.status(201).json({
-        success: true,
-        message: "Product created successfully",
-        newProduct,
-      });*/
-      ;
-    } catch (err) {
-      res.status(500).json({
-        success: false,
-      });
-    }
-  }
 
 async function removeProductFromCart() {}
 
@@ -166,7 +118,6 @@ module.exports = {
   createProduct,
   getProducts,
   getProductByName,
-  createNewProduct,
   updateProduct,
   deleteProduct,
   removeProductFromCart,
--- routes/product-routes.js
+++ routes/product-routes.js
@@ -5,16 +5,14 @@
 //delete product if need be
 
 const express = require('express');
-const { createProduct, getProducts, getProductByName, createNewProduct, updateProduct, deleteProduct } = require('../controllers/product.controller');
+const { createProduct, getProducts, getProductByName, updateProduct, deleteProduct } = require('../controllers/product.controller');
 const { protect } = require('../middleware/auth.middleware');
 
 const productRouter = express.Router();
 
-productRouter.post('/create',  createProduct);
-productRouter.post('/createNewCart',  createNewProduct);
+productRouter.post('/create', protect, createProduct);
 productRouter.get('/all', protect, getProducts);
 productRouter.get('/:name',  getProductByName);
-//productRouter.post('/addToCart', protect, addProductToCart);
 productRouter.patch('/update/:id', protect,updateProduct);
 productRouter.delete('/delete/:id', protect, deleteProduct);
 
